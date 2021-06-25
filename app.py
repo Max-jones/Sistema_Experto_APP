1bc48cd872199007a5cbf2fc85dfb8d82908e8fc
Max-jones <maximilianojones@gmail.com>
2021-06-24 20:34:18 -0400

    updating requirements.txt

--- app.py
+++ app.py
@@ -40,7 +40,6 @@ def load_data():
     # data[str(year_selected)]
     return data
 
-
 # CREATING FUNCTION FOR MAPS
 
 def map(data, lat, lon, zoom):
@@ -68,10 +67,17 @@ def map(data, lat, lon, zoom):
 
 # LAYING OUT THE TOP SECTION OF THE APP
 
-st.title("Sistema Experto - Visualizaciones - Pruebas")
+# st.title("Plataforma Web")
+'''
+# Sistema Experto - Plataforma WEB para detección de anomalías
+## Cargar el dataset a procesar
 
-zoom_selected = st.slider("Zoom del mapa", 10 , 16)
-uploaded_file = st.file_uploader("Choose a file")
+'''
+
+# Sección de carga del archivo .csv
+
+# Widget para cargar el archivo
+uploaded_file = st.file_uploader("Selecciona un archivo .csv ")
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
@@ -93,17 +99,46 @@ else:
         st.error('Por favor cargue un archivo .csv compatible')
         # raise KeyError('Por favor cargue un archivo .csv compatible')
 
+# Visualización previa del Dataset
+'''
+## Gráficos por variable
+'''
 
-row1_1, row1_2 = st.beta_columns((2,3))
+row1_1, row1_2, row1_3 = st.beta_columns((2,2,2))
 
 with row1_1:
     # st.title("Sistema Experto - Visualizaciones - Pruebas")
     # zoom_selected = st.slider("Zoom", 10 , 20)
     # # year_selected = st.slider("Select year", 2014, 2017)
     # year_selected = 2014
-    fig2 = px.line(datas, y='Temperatura [°C]', title='Temperatura')
+    fig1 = px.line(datas, y='Pression [cm H2O]', title='Presión [cm H2O]')
     # px.line()
 
+    fig1.update_xaxes(
+        rangeslider_visible=True,
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1, label="1m", step="month", stepmode="backward"),
+                dict(count=6, label="6m", step="month", stepmode="backward"),
+                dict(count=1, label="YTD", step="year", stepmode="todate"),
+                dict(count=1, label="1y", step="year", stepmode="backward"),
+                dict(step="all")
+            ])
+        )
+    )
+    st.plotly_chart(fig1, use_container_width=True) 
+with row1_2:
+    # st.write(
+    # """
+    # ##
+
+    # **Examinando las visualizaciones y un mapa**
+
+    # """)
+    # st.dataframe(data=datas.describe())
+    fig2 = px.line(datas, y='Temperatura [°C]', title='Temperatura [°C]')
+    # px.line()
+  
     fig2.update_xaxes(
         rangeslider_visible=True,
         rangeselector=dict(
@@ -113,64 +148,15 @@ with row1_1:
                 dict(count=1, label="YTD", step="year", stepmode="todate"),
                 dict(count=1, label="1y", step="year", stepmode="backward"),
                 dict(step="all")
-            ]))
+            ])
+        )
     )
     st.plotly_chart(fig2, use_container_width=True) 
-with row1_2:
-    st.write(
-    """
-    ##
-
-    **Examinando las visualizaciones y un mapa**
-
-    """)
-    st.dataframe(data=datas.describe())
-
-
-
-
-# FILTERING DATA BY HOUR SELECTED
-# data = data[data.index.year == year_selected]
-
-# LAYING OUT THE MIDDLE SECTION OF THE APP WITH THE MAPS
-#  row2_1, row2_2, row2_3, row2_4 = st.beta_columns((2,1,1,1))
-row2_1, row2_2, = st.beta_columns((2,3))
-
-# SETTING THE ZOOM LOCATIONS FOR THE AIRPORTS
-horcon= [-32.723230,-71.466365,15]
-# midpoint
-
-with row2_1:
-#     st.write("**All New York City from %i:00 and %i:00**" % (hour_selected, (hour_selected + 1) % 24))
-#     map(data, midpoint[0], midpoint[1], 11)
-    # st.write("**Horcón**")
-    # map(data, horcon[0],horcon[1], zoom_selected)
-
-    # x = [1, 2, 3, 4, 5]
-    # y = [6, 7, 2, 4, 5]
-    # p = figure(
-    #     title='simple line example',
-    #     x_axis_label='x',
-    #     y_axis_label='y')
-    # p.line(x, y, legend_label='Trend', line_width=2)
-    # st.bokeh_chart(p, use_container_width=True)
-    # Add histogram data
-    x1 = np.random.randn(200) - 2
-    x2 = np.random.randn(200)
-    x3 = np.random.randn(200) + 2
-    
-    # Group data together
-    hist_data = [x1]
-    
-    group_labels = ['Group 1', 'Group 2', 'Group 3']
-    
-    # Create distplot with custom bin_size
-    # fig = ff.create_distplot(
-    #     # hist_data, group_labels, bin_size=[.1, .25, .5])
-    fig = px.line(datas, y='Pression [cm H2O]', title='Presión')
+with row1_3:
+    fig3 = px.line(datas, y='EC [µs/cm]', title='Conductividad Eléctrica [µs/cm]')
     # px.line()
 
-    fig.update_xaxes(
+    fig3.update_xaxes(
         rangeslider_visible=True,
         rangeselector=dict(
             buttons=list([
@@ -179,20 +165,49 @@ with row2_1:
                 dict(count=1, label="YTD", step="year", stepmode="todate"),
                 dict(count=1, label="1y", step="year", stepmode="backward"),
                 dict(step="all")
-            ]))
+            ])
+        )
     )
+    st.plotly_chart(fig3, use_container_width=True)  
+
+
+
+# FILTERING DATA BY HOUR SELECTED
+# data = data[data.index.year == year_selected]
+
+# LAYING OUT THE MIDDLE SECTION OF THE APP WITH THE MAPS
+#  row2_1, row2_2, row2_3, row2_4 = st.beta_columns((2,1,1,1))
+row2_1, row2_2 = st.beta_columns((2,3))
+
+# SETTING THE ZOOM LOCATIONS FOR THE LOCATION SITE
+
+# midpoint
+
+with row2_1:
     
-    # fig.show()    
-    
-    # Plot!
-    st.plotly_chart(fig, use_container_width=True)    
+    '''
+    ##
+
+    **Examinando las estadísticas y un mapa(inventado).**
+
+    '''
+    zoom_selected = st.slider("Zoom del mapa", 10 , 16)
+
+    st.write('Descripción estadística del dataset cargado.')
+    st.write(datas[["Pression [cm H2O]","Temperatura [°C]","EC [µs/cm]"]].describe())
+    horcon= [-32.723230,-71.466365,15]
+  
+     
+    st.write('Datos disponibles',datas.columns.to_list()) 
      
 
 with row2_2:
     # st.dataframe(datas)
-    df = pd.DataFrame(
+    map_points = pd.DataFrame(
         np.random.randn(10, 2) / [150, 150] + [-32.723230,-71.466365],
         columns=['lat', 'lon'])
-    st.map(df,zoom=zoom_selected)
+    st.map(map_points,zoom=zoom_selected)
 
-
+'''
+## Detección de anomalías
+'''
