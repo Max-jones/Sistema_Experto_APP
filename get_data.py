162e6ca8b9be4cc95203ff833dc8b378c9ac00e0
Max-jones <maximilianojones@gmail.com>
2021-10-20 22:09:25 -0300

    actualizando repositorio

--- get_data.py
+++
@@ -1,8 +0,0 @@
-import json
-# import pandas as pd
-
-path = "C:\\Users\\elmha\\OneDrive - Universidad de Chile\\GitHub\\Sistema_Experto_APP\\myfile.json"
-
-_json = json.load(open(path))
-
-df = pd.DataFrame(_json['series'][0]['values'],columns=_json['series'][0]['columns'])
