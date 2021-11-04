c4611225f65c44c1afce965f3853789d414cb3b4
Max-jones <maximilianojones@gmail.com>
2021-11-04 12:17:14 -0300

    cambios

--- load_local_data.py
+++ load_local_data.py
@@ -1,6 +1,18 @@
-from app import load_data
+import pandas as pd
+import pytz
 
-local_path = 'data/Horcon-etiquetado_con_1_etiqueta.csv'
+local_pathh = 'C:\\Users\\elmha\\OneDrive - Universidad de Chile\\GitHub\\Sistema_Experto_APP\\data\Horcon-etiquetado_con_1_etiqueta.csv'
+def load_data(path):
+    """
+    ARGS: path to the local .csv file
+    Load data and search for the Date_Time column to index the dataframe by a datetime value.
 
-df = load_data(local_path)
+    """
+    data = pd.read_csv(path,delimiter=";")  # , engine='python')
+    data["Date_Time"] = pd.to_datetime(data["Date_Time"])
+    data.set_index("Date_Time", inplace=True)
+    chile = pytz.timezone("Chile/Continental")
+    data.index = data.index.tz_localize(pytz.utc).tz_convert(chile)
+    return data
+
 
