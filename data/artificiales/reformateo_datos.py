f7036f36a5944e977ecdcefa5d52144b0146f74f
Max-jones <maximilianojones@gmail.com>
2022-01-11 22:32:42 -0300

    major changes

---
+++ data/artificiales/reformateo_datos.py
@@ -0,0 +1,29 @@
+# %%%% 
+import pandas as pd
+p1 = pd.read_csv('C:\\Users\\elmha\OneDrive - Universidad de Chile\GitHub\Sistema_Experto_APP\data\\artificiales\Pozo_1_Labeled.csv',sep=';')
+
+p2 = pd.read_csv('C:\\Users\\elmha\OneDrive - Universidad de Chile\GitHub\Sistema_Experto_APP\data\\artificiales\Pozo_2_Labeled.csv',sep=';')
+
+p3 = pd.read_csv('C:\\Users\\elmha\OneDrive - Universidad de Chile\GitHub\Sistema_Experto_APP\data\\artificiales\Pozo_3_Labeled.csv',sep=';')
+
+# %%
+
+p = [p1,p2,p3]
+i=1
+for pozo in p:
+    print('procesando pozo ',i)
+    pozo['Date_Time'] = pozo['Date'] + ' ' + pozo['Time']
+    pozo.drop(columns=['Date','Time'],inplace=True)# %%
+    name = 'pozo_'+str(i)+'_procesado.csv'
+    print(name)
+    pozo["Date_Time"] = pd.to_datetime(pozo["Date_Time"])
+    pozo.set_index("Date_Time", inplace=True)
+    pozo.applymap(lambda x: str(x.replace(',','.')))
+    pozo.to_csv(name,sep=';')
+
+    i+=1
+# %%
+pozo
+# %%
+df
+# %%
