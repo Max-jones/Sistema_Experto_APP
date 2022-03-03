a100446c2a6c420954638ba1fd60514e3d673e4c
Max-jones <maxjones@ug.uchile.cl>
2022-03-03 16:08:32 -0300

    reformateo datos ajustado

--- data/artificiales/reformateo_datos.py
+++ data/artificiales/reformateo_datos.py
@@ -1,12 +1,16 @@
 # %%%% 
+from timeit import timeit
 import pandas as pd
-p1 = pd.read_csv('C:\\Users\\elmha\OneDrive - Universidad de Chile\GitHub\Sistema_Experto_APP\data\\artificiales\Pozo_1_Labeled.csv',sep=';')
+# raiz_ntbk = 'C:\\Users\\elmha\OneDrive - Universidad de Chile\GitHub\Sistema_Experto_APP\data\\artificiales\'
+raiz = "C:\\Users\\Universidad de Chile\Documents\\GitHub\\Sistema_Experto_APP\\data\\artificiales\\"
+# %%%% 
+%timeit
+p1 = pd.read_csv(raiz+'Pozo_1_Labeled.csv',sep=';')
 
-p2 = pd.read_csv('C:\\Users\\elmha\OneDrive - Universidad de Chile\GitHub\Sistema_Experto_APP\data\\artificiales\Pozo_2_Labeled.csv',sep=';')
+p2 = pd.read_csv(raiz+'Pozo_2_Labeled.csv',sep=';')
 
-p3 = pd.read_csv('C:\\Users\\elmha\OneDrive - Universidad de Chile\GitHub\Sistema_Experto_APP\data\\artificiales\Pozo_3_Labeled.csv',sep=';')
+p3 = pd.read_csv(raiz+'Pozo_3_Labeled.csv',sep=';')
 
-# %%
 
 p = [p1,p2,p3]
 i=1
@@ -18,12 +22,15 @@ for pozo in p:
     print(name)
     pozo["Date_Time"] = pd.to_datetime(pozo["Date_Time"])
     pozo.set_index("Date_Time", inplace=True)
-    pozo.applymap(lambda x: str(x.replace(',','.')))
+    # print(pozo)
+    # pozo.apply(lambda x: str(x.replace(',','.')))
+    pozo.replace(',','.',regex=True,inplace=True)
     pozo.to_csv(name,sep=';')
 
     i+=1
 # %%
-pozo
+%%timeit
+# pozo
 # %%
-df
+# df
 # %%
